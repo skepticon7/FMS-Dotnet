57067c4b0151061d3e0a91558aff876a02a5a709
skepticon <blackamvs7@gmail.com>
2025-11-02 18:10:15 +0100

    added yarp as an api gateway and successfully tested the forecast api with it , all was done through docker containers

---
+++ backend/ApiGateway/YarpGateway.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+
+app.Run();
+
